57453671a6473b60d6d981837e113f7ee84f005d
Jonathan Gonzalez <jonathangonzalez@Jonathans-MacBook-Pro-2.local>
2021-04-03 16:32:13 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.scss';
+
+const App = () => <div className="test">Smash that subscribe button</div>;
+
+ReactDOM.render(<App />, document.getElementById('main'));
